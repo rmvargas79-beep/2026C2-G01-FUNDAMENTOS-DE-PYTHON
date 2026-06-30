b1f9be7eda0d89f5af1290575d8ad157a47e5cab
rmvargas79-beep <rmvargas79@gmail.com>
2026-06-29 19:40:16 -0600

    Clase7

--- NOTEBOOKS/Clase_07/clase07_analisis_emprendimientos.py
+++ NOTEBOOKS/Clase_07/clase07_analisis_emprendimientos.py
@@ -6,3 +6,66 @@ sede usando listas, diccionarios, funciones, ciclos y condicionales.
 
 from sedes import sedes
 
+
+def calcular_total(ventas):
+    """_Recibo una lista, la sumo y retorno el total.
+
+    Args:
+        ventas (list): Lista con las ventas del emprendimiento
+
+    Returns:
+        float: Sumatoria total de Ventas
+    """
+    return sum(ventas)
+
+def calcular_promedio(lista):
+    """Retorna el promedio de ventas de una Lista"""
+    return sum(lista) / len(lista)
+
+def calcular_procentaje(total,ventas, formato = False):
+    porcentaje = total / meta * 100
+    if formato:
+        return f"{total / meta * 100:.2f}%"
+    
+    return porcentaje
+
+def calcular_clasificacion(total, meta):
+    porcentaje = calcular_porcetaja(total, meta)
+    if porcentaja_sede >= 100:
+        mensaje_sede = "Meta alcanzada."
+    elif porcentaja_sede >= 80:
+        mensaje_sede = "Meta casi alcanzada, prestar atencion."
+    else:
+        "Meta no alcanzada URGE ATENCION"
+
+reporte = []
+
+for sede in sedes:
+    print(sedes)
+    print("Cantidad de sedes: ", len(sedes))
+    print ("Tipo Variables sedes:", type(sedes[0]))
+    print("Datos por sede: ", sedes[0].keys())
+    print("Primera sede: ", sedes[0])
+    print("Nombre Primera sede: ", sedes[0]["nombre"])
+
+
+
+    sede_demo = sedes[0]
+    ventas = sede_demo["ventas"]
+    meta= sede_demo["meta"]
+    total_sede = calcular_total(ventas)
+    promedio_sede = calcular_promedio(ventas)
+    porcentaja_sede = calcular_procentaje(total_sede,meta, True)
+    estado = calcular_clasificacion(total_sede,meta)
+
+
+    print(porcentaja_sede, total_sede)
+
+
+
+
+#print(imprimir_reporte(reporte))
+#MAS ingresos
+#Provincias
+
+

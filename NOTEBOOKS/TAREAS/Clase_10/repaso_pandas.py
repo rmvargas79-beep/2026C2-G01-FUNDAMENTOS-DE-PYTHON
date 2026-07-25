b74e2023b25a6ddcf85b8c07486cbcfabb8d0eb1
rmvargas79-beep <rmvargas79@gmail.com>
2026-07-25 09:20:23 -0600

    .

--- NOTEBOOKS/TAREAS/Clase_10/repaso_pandas.py
+++ NOTEBOOKS/TAREAS/Clase_10/repaso_pandas.py
@@ -1,1 +1,21 @@
 #Repaso Tutoria 25/07/2026
+#pip install pandas
+
+import pandas as pd
+
+pts = 42
+datos = {
+    "estudiante": ["Pamela", "Ronald", "Maria", "Luis"],
+    "puntos": [40, 36, 25, 20],
+    "edad":[20, 25, 19, 30]
+        
+}
+
+estudiantes = pd.DataFrame(datos)
+print(estudiantes.shape)
+print(estudiantes)
+
+
+estudiantes["nota"] = (estudiantes["puntos"] * 100) / pts
+
+print(estudiantes)

d37cbda794ece3db042d6cb1a3e446425a7f0bdc
rmvargas79-beep <rmvargas79@gmail.com>
2026-07-27 19:05:57 -0600

    menu

--- NOTEBOOKS/Clase 11/app.py
+++ NOTEBOOKS/Clase 11/app.py
@@ -7,6 +7,34 @@ from limpieza_datos import limpiar_datos
 def ejecutar():
     datos_crudos = cargar_tabla_bccr(URL_BCCR)
     datos = limpiar_datos(datos_crudos)
+    print("Datos cargados exitosamente de https://gee.bccr.fi.cr")
+    while True:
+        print("\nPROYECTO DE ANALISIS BCCR")
+        print("1. Mostrar primeras entidades Limpias")
+        print("2. Mostrar entidades con diferencial superior al promedio")
+        print("3. Promedio por tipo entidad")
+        print("4. Mostrar lista de entidades")
+        print("5. Graficar")
+        print("6. Salir")
+        
+        opcion = input("Ingrese la opcion del Menu: ").lower().strip()
+        if opcion == "1":
+            datos.info()
+            print(datos.head())
+        elif opcion == "2":
+            pass
+        elif opcion == "3":
+            pass
+        elif opcion == "4":
+            pass
+        elif opcion == "5":
+            pass
+        elif opcion == "6":
+            print("\nAnalisis Finalizado")
+            input("Presione Enter para Salir...")
+            break
+        else:
+            print("Opcion invalida. Escriba un numero del 1 al 6.")
     datos.info()
     print(datos_crudos.head())
     print(datos.head())

a1ba0e3406c2c14459d632f6dcd334d59ca6ea9e
rmvargas79-beep <rmvargas79@gmail.com>
2026-08-10 21:25:45 -0600

    .

--- Projecto_Final/lectura_datos.py
+++ Projecto_Final/lectura_datos.py
@@ -45,6 +45,8 @@ def leer_archivos_csv():
         df.columns = df.columns.str.strip()
         
         df.rename(columns={"Precio venta": "Precio Venta"},inplace=True)
+        df.rename(columns={"Precio venra": "Precio Venta"},inplace=True)
+        #df.rename(columns={df.columns[5]: "Precio Venta"}, inplace=True)
 
         # Eliminar columnas vacías
         df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
